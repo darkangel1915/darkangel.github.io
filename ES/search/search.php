38210a5cce77e3acbebc4a3d06a43545218b405c
nikolasmurdock <nikolasmurdock@gmail.com>
2016-11-01 15:59:45 -0600

    The uploading everything commit

---
+++ ES/search/search.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no"/>
+    <link rel="icon" href="../images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="search.css"/>
+    <link rel="stylesheet" href="../css/grid.css"/>
+    <link rel="stylesheet" href="../css/search-form.css"/>
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="../js/jquery.js"></script>
+    <script src="../js/jquery-migrate-1.2.1.js"></script>
+    <script src="./search.js"></script>
+    <!--[if lt IE 9]>
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+            <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0"
+                 height="42" width="820"
+                 alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+        </a>
+    </div>
+    <script src="../js/html5shiv.js"></script>
+    <script src="../js/selectivizr.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="../css/ie.css">
+
+    <![endif]-->
+</head>
+<body>
+<div class="page">
+    <!--============================== Header =================================-->
+    <header id="header">
+        <div id="stuck_container">
+            <div class="bg-header_2">
+                <div class="container">
+                    <div class="row">
+                        <div class="grid_12">
+
+                            <div class="put-left logo-caption">
+                                <h1>
+                                    <a href="../index.html">J.Ohira</a>
+                                </h1>
+
+                                <div class="second-word"><p>photographer</p></div>
+
+                            </div>
+
+                            <div class="put-right social-list">
+                                <ul>
+                                    <li class="color_1">
+                                        <a class="fa fa-facebook" href="#"></a>
+                                    </li>
+                                    <li class="color_1">
+                                        <a class="fa fa-google-plus" href="#"></a>
+                                    </li>
+                                    <li class="color_1">
+                                        <a class="fa fa-rss" href="#"></a>
+                                    </li>
+                                    <li class="color_1">
+                                        <a class="fa fa-pinterest" href="#"></a>
+                                    </li>
+                                    <li class="last color_1">
+                                        <a class="fa fa-linkedin" href="#"></a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <nav class="put-right header-menu">
+                                <ul class="sf-menu">
+                                    <li class="first-item">
+                                        <a href="../index.html">Home</a>
+                                    </li>
+                                    <li>
+                                        <a href="../index-1.html">About</a>
+                                        <ul>
+                                            <li>
+                                                <a href="#">History</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">News</a>
+                                                <ul>
+                                                    <li>
+                                                        <a href="#">Latest</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#">Archive</a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                            <li>
+                                                <a href="#">Testimonials</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="../index-2.html">Services</a>
+                                    </li>
+                                    <li>
+                                        <a href="../index-3.html">works</a>
+                                    </li>
+                                    <li>
+                                        <a href="../index-4.html">Blog</a>
+                                    </li>
+                                    <li class="last-item">
+                                        <a href="../index-5.html">Contacts</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </header>
+
+    <!--============================ Content ================================-->
+    <div id="content">
+        <div class="wrapper_5 last-child">
+            <div class="container">
+                <div class="row">
+                    <div class="grid_12">
+
+                        <div class="heading_8 text_3 color_5 h3-wrap">
+                            <h3>Search results</h3>
+                        </div>
+
+                        <div id="search-results"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="divider"></div>
+    </div>
+</div>
+
+<!--======================== Footer =================================-->
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="box">
+                <div class="grid_12">
+                    <div class="heading text_9 color_10">
+                        <h4>LIKE WHAT YOU SEE? GET IN TOUCH WITH ME.</h4>
+
+                        <p class="color_8 text_10">If you want to see more recent works, browse my
+                            <a class="color_1" href="#">Portfolio</a>
+                        </p>
+                    </div>
+
+                    <div class="social-list">
+                        <ul>
+                            <li>
+                                <a class="fa fa-twitter" href="#"></a>
+                            </li>
+                            <li>
+                                <a class="fa fa-facebook" href="#"></a>
+                            </li>
+                            <li>
+                                <a class="fa fa-pinterest" href="#"></a>
+                            </li>
+                            <li>
+                                <a class="fa fa-google-plus" href="#"></a>
+                            </li>
+                            <li>
+                                <a class="fa fa-github" href="#"></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <p class="copyright color_8">
+                        <span>&copy;
+                            <span id="copyright-year"></span></span>
+                        <a href="../index-6.html">Privacy policy</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<div id="toTop"><span class="fa fa-chevron-up"></span></div>
+<script src="script.js"></script>
+</body>
+</html>
